e0dcbe1a5f06dc5396ef2d6f9dc0ee89277ea002
AlexseyWeb <alongenet32@gmail.com>
2024-11-30 19:40:48 +0300

    Insert data in db

--- shop/models.py
+++ shop/models.py
@@ -10,9 +10,9 @@ class Category(models.Model):
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=300)
     price = models.FloatField()
     students_qty = models.IntegerField()
-    revies_qty = models.IntegerField
+    reviews_qty = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
