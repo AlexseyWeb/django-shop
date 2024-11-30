756200371ded16fd37da231e2f934545721ba1ba
AlexseyWeb <alongenet32@gmail.com>
2024-11-30 21:05:39 +0300

    added templates

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Course
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>Hello from the shop app!</h1>")
+    courses = Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
